d8879797ca4661ee07ac5ef4335922b76ffa2b46
Julie <sizumasa@hotmail.com>
2019-03-19 13:42:35 +0800

    native 跟 FB login layout 打架

--- G2-Front/js/lib.js
+++ G2-Front/js/lib.js
@@ -152,10 +152,7 @@ app.fb.statusChangeCallback=function(response){
 		app.fb.testAPI();
 		app.fb.updateLoginToServer(response);
 	}else{
-		// 沒登入，或情況不明，給使用者看登入按鈕
-        // if ( window.location.href.indexOf("profile") > -1 && app.state.auth!=='native') {
-        //     app.initProfile(null);
-		// }
+		// 沒登入，或情況不明
 		console.log('fb未登入');
 	}
 };
