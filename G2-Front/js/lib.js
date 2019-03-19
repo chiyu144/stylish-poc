c11714cb2f4069a9239fa63359caa1e2f362228e
Julie <sizumasa@hotmail.com>
2019-03-19 15:48:35 +0800

    profile layout 改良

--- G2-Front/js/lib.js
+++ G2-Front/js/lib.js
@@ -168,7 +168,6 @@ app.fb.updateLoginToServer=function(response){
 		}else{
 			console.log("fb 登入成功", result);
 			localStorage.setItem("stylish_login", JSON.stringify(result));
-			window.location = './profile.html';
 		}
 	});
 };
